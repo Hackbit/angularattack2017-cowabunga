1137a7e0d709788a1b8497394b9bd2e96b27c094
tu4mo <tu4mo@users.noreply.github.com>
2017-04-22 22:57:23 +0300

    Adjust login for bigger screens

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ import GithubAuthProvider = firebase.auth.GithubAuthProvider;
 @Component({
   selector: 'cowabunga-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
