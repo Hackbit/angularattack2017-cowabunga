84af4646b0aaea821178eed7135ce8e2bdf5a562
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-04-22 12:31:36 +0300

    Populate user data from firebase.

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Achievement } from '../achievement';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { User } from '../user';
 
 @Component({
   selector: 'cowabunga-my-profile',
@@ -8,10 +10,13 @@ import { Achievement } from '../achievement';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
-  constructor(private firebase: AngularFireDatabase) {
+  user: User;
+
+  constructor(private database: AngularFireDatabase, private auth: AngularFireAuth) {
   }
 
   ngOnInit() {
+    this.database.object(`users/${this.auth.auth.currentUser.uid}`).subscribe(user => this.user = user);
   }
 
 }
