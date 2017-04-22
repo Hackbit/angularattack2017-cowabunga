0a3a67b209da40cbd605c08044e4661cd43370c8
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-04-22 15:42:47 +0300

    Implement back button logic.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'app/user.service';
 import { User } from 'app/user';
 import { Observable } from 'rxjs/Observable';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'cowabunga-header',
@@ -11,10 +12,14 @@ import { Observable } from 'rxjs/Observable';
 export class HeaderComponent implements OnInit {
   user: Observable<User>;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private location: Location) { }
 
   ngOnInit() {
     this.user = this.userService.getUser();
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
