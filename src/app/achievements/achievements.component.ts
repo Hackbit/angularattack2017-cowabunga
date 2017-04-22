3b2a6dda20ee5e7785cfc094824e1733b4a7f110
Roope Hakulinen <roope.hakulinen@gmail.com>
2017-04-22 16:59:46 +0300

    Implemented selection of type for achievements list.

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Achievement } from '../achievement';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'cowabunga-achievements',
@@ -10,8 +11,12 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AchievementsComponent implements OnInit {
   achievements: Observable<Achievement[]>;
+  type: string;
 
-  constructor(private firebase: AngularFireDatabase) {
+  constructor(private firebase: AngularFireDatabase, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(({ type = 'Near by'}) => {
+      this.type = type;
+    });
   }
 
   ngOnInit() {
