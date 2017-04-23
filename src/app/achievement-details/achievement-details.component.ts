b8d2b33846d905436efd6090af27fd740eebab60
hollannikas <hollannikas@users.noreply.github.com>
2017-04-23 08:36:09 +0300

    Do not show check-in button if user is already checked in here

--- src/app/achievement-details/achievement-details.component.ts
+++ src/app/achievement-details/achievement-details.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Achievement } from '../achievement';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/from';
 
 @Component({
   selector: 'cowabunga-achievement-details',
@@ -11,16 +18,34 @@ import { ActivatedRoute } from '@angular/router';
 export class AchievementDetailsComponent implements OnInit {
 
   achievement: Achievement;
+  userCheckedIn: boolean;
 
-  constructor(private firebase: AngularFireDatabase, private route: ActivatedRoute) { }
+  constructor(private firebase: AngularFireDatabase,
+              private route: ActivatedRoute,
+              private userService: UserService) {
+  }
 
   ngOnInit() {
     this.route.params
       .map(params => params['id'])
       .subscribe(id => {
         this.firebase.object(`achievements/${id}`)
-          .subscribe(achievement => this.achievement = achievement);
+          .do(achievement => this.achievement = achievement)
+          .switchMap(() => {
+            return this.userService.getUser()
+              .map(user => this.extractCheckinIds(user))
+              .switchMap(checkIns => Observable.from(checkIns))
+              .filter(checkIn => checkIn.achievement === this.achievement.$key);
+          })
+          .subscribe(() => this.userCheckedIn = true);
       });
+  }
+
+  private extractCheckinIds(user) {
+    const checkIns = [];
+    Object.keys(user.checkIns)
+      .forEach(key => checkIns.push(user.checkIns[key]));
+    return checkIns;
   }
 
 }
