6358e196fe31074c352c0c6221f6bc85e293b1f2
hollannikas <hollannikas@users.noreply.github.com>
2017-04-23 17:59:26 +0300

    Show last 10 check-ins in feed, sorted by date

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -14,10 +14,23 @@ export class FeedComponent implements OnInit {
   constructor(private afDatabase: AngularFireDatabase) { }
 
   ngOnInit() {
-    this.checkIns = this.afDatabase.list('checkInFeed')
-      .map(checkIns => {
+    this.checkIns = this.afDatabase.list('checkInFeed', {
+      query: {
+        orderByChild: 'date',
+        limitToLast: 10
+      }
+    }).map(checkIns => {
         const arr = [];
         Object.keys(checkIns).forEach(key => arr.push(checkIns[key]));
+        arr.sort((a, b) => {
+          if (a.date > b.date) {
+            return -1;
+          }
+          if (a.date < b.date) {
+            return 1;
+          }
+          return 0;
+        });
         return arr;
       });
   }
